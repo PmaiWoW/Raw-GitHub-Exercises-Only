ced8fd48113f5662b2d766ee055f6c3e87b17539
PmaiWoW <PmaiWoW@Gmail.com>
2019-06-08 13:03:10 +0100

    Add test code for Raw exs 56 and 57 (numbers may change)

---
+++ ex56_poo/Problem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex56_poo
+{
+    public class Problem
+    {
+        public static void Main()
+        {
+            // Um array de objetos de diferentes tipos
+            object[] stuff = { "ola", 1, 2.3, 5f, 12L, 4UL, 5U, "bye", 4, 9 };
+            // Imprimir apenas objetos do tipo inteiro
+            foreach (int i in Filter<object, int>(stuff))
+            {
+                Console.WriteLine("int = " + i);
+            }
+            // Imprimir apenas objetos do tipo float
+            foreach (float f in Filter<object, float>(stuff))
+            {
+                Console.WriteLine("float = " + f);
+            }
+        }
+
+        static IEnumerable<Y> Filter<X, Y>(IEnumerable<object> stuff)
+        {
+            List<Y> returnList = new List<Y>();
+
+            foreach (object o in stuff)
+            {
+                if (o is Y) returnList.Add((Y)o);
+            }
+            return returnList;
+        }
+
+
+    }
+}
---
+++ ex57_poo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex57_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            List<int> intList = new List<int>();
+            List<string> stringList = new List<string>();
+
+            Console.WriteLine("How many times to copy the value?");
+            num = int.Parse(Console.ReadLine());
+
+            intList = CopyValToList<int>(10, num);
+            foreach(int val in intList) { Console.WriteLine(val); }
+
+            Console.WriteLine("How many times to copy the value?");
+            num = int.Parse(Console.ReadLine());
+
+            stringList = CopyValToList<string>("bruh.jpeg", num);
+            for(int i = 0; i < num; i++) { Console.WriteLine(stringList[i]); }
+        }
+
+        static List<T> CopyValToList<T>(T val, int num)
+        {
+            List<T> genericList = new List<T>();
+            for(int i = 0; i < num; i++)
+            {
+                genericList.Add(val);
+            }
+            return genericList;
+        }
+    }
+}
